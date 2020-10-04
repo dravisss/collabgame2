0e1eb50057e887f0971c247e2ae6574cbc43ae70
Ravi Resck <ravi@collabdesign.org>
2020-10-04 23:18:18 +0100

    Update

---
+++ objects.tsx
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="objects" tilewidth="384" tileheight="332" tilecount="62" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="160" height="192" source="../tiled/examples/sticker-knight/map/alter.png"/>
+ </tile>
+ <tile id="1">
+  <image width="135" height="115" source="../tiled/examples/sticker-knight/map/backgroundArch.png"/>
+ </tile>
+ <tile id="2">
+  <image width="380" height="140" source="../tiled/examples/sticker-knight/map/backgroundMountain.png"/>
+ </tile>
+ <tile id="3">
+  <image width="129" height="332" source="../tiled/examples/sticker-knight/map/backgroundTower.png"/>
+ </tile>
+ <tile id="4">
+  <image width="109" height="177" source="../tiled/examples/sticker-knight/map/backgroundTree.png"/>
+ </tile>
+ <tile id="5">
+  <image width="96" height="64" source="../tiled/examples/sticker-knight/map/blobBlue.png"/>
+ </tile>
+ <tile id="6">
+  <image width="96" height="64" source="../tiled/examples/sticker-knight/map/blobGreen.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../tiled/examples/sticker-knight/map/blue.png"/>
+ </tile>
+ <tile id="8">
+  <image width="96" height="128" source="../tiled/examples/sticker-knight/map/bombStroked.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../tiled/examples/sticker-knight/map/castleWall.png"/>
+ </tile>
+ <tile id="10">
+  <image width="384" height="128" source="../tiled/examples/sticker-knight/map/cloud.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="192" source="../tiled/examples/sticker-knight/map/column1.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="192" source="../tiled/examples/sticker-knight/map/column2.png"/>
+ </tile>
+ <tile id="13">
+  <image width="160" height="192" source="../tiled/examples/sticker-knight/map/doorBlueStroked.png"/>
+ </tile>
+ <tile id="14">
+  <image width="160" height="192" source="../tiled/examples/sticker-knight/map/doorGreenStroke.png"/>
+ </tile>
+ <tile id="15">
+  <image width="160" height="192" source="../tiled/examples/sticker-knight/map/doorRedStroked.png"/>
+ </tile>
+ <tile id="16">
+  <image width="160" height="192" source="../tiled/examples/sticker-knight/map/doorStroked.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="../tiled/examples/sticker-knight/map/earthWall.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="../tiled/examples/sticker-knight/map/earthWall2.png"/>
+ </tile>
+ <tile id="19">
+  <image width="160" height="192" source="../tiled/examples/sticker-knight/map/exit.png"/>
+ </tile>
+ <tile id="20">
+  <image width="192" height="192" source="../tiled/examples/sticker-knight/map/flare.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="../tiled/examples/sticker-knight/map/gemBlueStroked.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="../tiled/examples/sticker-knight/map/gemRedStroked.png"/>
+ </tile>
+ <tile id="23">
+  <image width="224" height="32" source="../tiled/examples/sticker-knight/map/grassLarge.png"/>
+ </tile>
+ <tile id="24">
+  <image width="160" height="32" source="../tiled/examples/sticker-knight/map/grassSmall.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="../tiled/examples/sticker-knight/map/grey.png"/>
+ </tile>
+ <tile id="26">
+  <image width="128" height="160" source="../tiled/examples/sticker-knight/map/hero.png"/>
+ </tile>
+ <tile id="27">
+  <image width="96" height="64" source="../tiled/examples/sticker-knight/map/keyGreenStroked.png"/>
+ </tile>
+ <tile id="28">
+  <image width="96" height="64" source="../tiled/examples/sticker-knight/map/keyRedStroked.png"/>
+ </tile>
+ <tile id="29">
+  <image width="96" height="64" source="../tiled/examples/sticker-knight/map/keyYellowStroked.png"/>
+ </tile>
+ <tile id="30">
+  <image width="192" height="64" source="../tiled/examples/sticker-knight/map/platform1.png"/>
+ </tile>
+ <tile id="31">
+  <image width="192" height="64" source="../tiled/examples/sticker-knight/map/platform2.png"/>
+ </tile>
+ <tile id="32">
+  <image width="192" height="64" source="../tiled/examples/sticker-knight/map/platform3.png"/>
+ </tile>
+ <tile id="33">
+  <image width="192" height="64" source="../tiled/examples/sticker-knight/map/platform4.png"/>
+ </tile>
+ <tile id="34">
+  <image width="256" height="96" source="../tiled/examples/sticker-knight/map/platformBase1.png"/>
+ </tile>
+ <tile id="35">
+  <image width="256" height="96" source="../tiled/examples/sticker-knight/map/platformBase2.png"/>
+ </tile>
+ <tile id="36">
+  <image width="256" height="96" source="../tiled/examples/sticker-knight/map/platformBase3.png"/>
+ </tile>
+ <tile id="37">
+  <image width="256" height="96" source="../tiled/examples/sticker-knight/map/platformBase4.png"/>
+ </tile>
+ <tile id="38">
+  <image width="128" height="96" source="../tiled/examples/sticker-knight/map/platformBlock1.png"/>
+ </tile>
+ <tile id="39">
+  <image width="128" height="96" source="../tiled/examples/sticker-knight/map/platformBlock2.png"/>
+ </tile>
+ <tile id="40">
+  <image width="128" height="96" source="../tiled/examples/sticker-knight/map/platformBlock3.png"/>
+ </tile>
+ <tile id="41">
+  <image width="128" height="96" source="../tiled/examples/sticker-knight/map/platformBlock4.png"/>
+ </tile>
+ <tile id="42">
+  <image width="96" height="64" source="../tiled/examples/sticker-knight/map/platformConnector1.png"/>
+ </tile>
+ <tile id="43">
+  <image width="96" height="64" source="../tiled/examples/sticker-knight/map/platformConnector2.png"/>
+ </tile>
+ <tile id="44">
+  <image width="96" height="64" source="../tiled/examples/sticker-knight/map/platformConnector3.png"/>
+ </tile>
+ <tile id="45">
+  <image width="96" height="64" source="../tiled/examples/sticker-knight/map/platformConnector4.png"/>
+ </tile>
+ <tile id="46">
+  <image width="96" height="96" source="../tiled/examples/sticker-knight/map/pushBlock1.png"/>
+ </tile>
+ <tile id="47">
+  <image width="96" height="96" source="../tiled/examples/sticker-knight/map/pushBlock2.png"/>
+ </tile>
+ <tile id="48">
+  <image width="96" height="96" source="../tiled/examples/sticker-knight/map/pushBlock3.png"/>
+ </tile>
+ <tile id="49">
+  <image width="64" height="64" source="../tiled/examples/sticker-knight/map/shadow.png"/>
+ </tile>
+ <tile id="50">
+  <image width="93" height="93" source="../tiled/examples/sticker-knight/map/shieldStroked.png"/>
+ </tile>
+ <tile id="51">
+  <image width="160" height="128" source="../tiled/examples/sticker-knight/map/sign.png"/>
+ </tile>
+ <tile id="52">
+  <image width="133" height="160" source="../tiled/examples/sticker-knight/map/skeleton.png"/>
+ </tile>
+ <tile id="53">
+  <image width="192" height="96" source="../tiled/examples/sticker-knight/map/swordStroked.png"/>
+ </tile>
+ <tile id="54">
+  <image width="96" height="96" source="../tiled/examples/sticker-knight/map/torch.png"/>
+ </tile>
+ <tile id="55">
+  <image width="128" height="32" source="../tiled/examples/sticker-knight/map/trap.png"/>
+ </tile>
+ <tile id="56">
+  <image width="128" height="96" source="../tiled/examples/sticker-knight/map/wallDecor1.png"/>
+ </tile>
+ <tile id="57">
+  <image width="128" height="96" source="../tiled/examples/sticker-knight/map/wallDecor2.png"/>
+ </tile>
+ <tile id="58">
+  <image width="160" height="128" source="../tiled/examples/sticker-knight/map/wallDecor3.png"/>
+ </tile>
+ <tile id="59">
+  <image width="65" height="64" source="../tiled/examples/sticker-knight/map/window1.png"/>
+ </tile>
+ <tile id="60">
+  <image width="64" height="64" source="../tiled/examples/sticker-knight/map/window2.png"/>
+ </tile>
+ <tile id="61">
+  <image width="56" height="59" source="../tiled/examples/sticker-knight/map/window3.png"/>
+ </tile>
+</tileset>
